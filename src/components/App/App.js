3a51a37b4848bf10cff8b6c0d76a29eaef41dda1
hazzeldorn <jonas@ohodesign.ch>
2024-08-06 18:14:49 +0200

    Revert "feat: vercel analytics added"
    This reverts commit 34acf20e0407077ba02542b21d77514b68a9e40b.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,14 +3,11 @@ import React from "react";
 import MemoryPlayground from "../MemoryPlayground";
 import Footer from "../Footer";
 
-import { Analytics } from "@vercel/analytics/react";
-
 function App() {
   return (
     <>
       <MemoryPlayground />
       <Footer />
-      <Analytics />
     </>
   );
 }
